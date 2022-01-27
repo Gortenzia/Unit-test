1219215da25a1622a4b908f0a8b92cd41fea2f2c
Gortenzia <tacumineka@gmail.com>
2022-01-27 14:30:59 +0300

    1

---
+++ src/js/__tests__/basic.test.js
@@ -0,0 +1,8 @@
+import function hp from '../basic';
+  
+
+test('Проверка здоровья персоонажа', () => {
+  const сharacter = { name: 'Маг', health: 90 };
+
+  expect(result).toBe(healthy);
+});
---
+++ src/js/basic.js
@@ -0,0 +1,10 @@
+function hp(character{ name, health }) {
+  const result;
+  if (character.health > 50) {
+    return result = healthy;
+  } else if (character.health < 50 && character.health > 15) {
+    return result = wounded;
+  } else if (character.health < 15) {
+    return result = critical;
+  }
+}
