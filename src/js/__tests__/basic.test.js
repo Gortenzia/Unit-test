e176d0f6df00c92c7425f10c2cd3381dfec79425
Gortenzia <tacumineka@gmail.com>
2022-01-29 00:08:14 +0300

    Снова зеленые тесты!

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -3,21 +3,21 @@ import heroes from '../basic.js';
 
 
 test('Проверка здоровья персоонажа', () => {
-  const health = 51;
+  const health = 80;
   const expected = 'healthy';
   const recevied = heroes(health);
   expect(recevied).toBe(expected);
 });
 
 test('health check', () => {
-  const health = 49;
+  const health = 42;
   const expected = 'wounded';
   const recevied = heroes(health);
   expect(recevied).toBe(expected);
 });
 
 test('health check', () => {
-  const health = 14;
+  const health = 9;
   const expected = 'critical';
   const recevied = heroes(health);
   expect(recevied).toBe(expected);
